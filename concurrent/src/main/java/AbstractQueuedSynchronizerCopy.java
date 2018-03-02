8ece99174460b92f5e5309d6ef765b9da0685418
weijin <weijin729@gmail.com>
2018-03-02 10:54:41 +0800

    cancelAcquire

--- concurrent/src/main/java/AbstractQueuedSynchronizerCopy.java
+++ concurrent/src/main/java/AbstractQueuedSynchronizerCopy.java
@@ -57,6 +57,9 @@ public class AbstractQueuedSynchronizerCopy extends AbstractOwnableSynchronizerC
   private volatile Node head;
   private volatile Node tail;
 
+  /**
+   * state用于设定可重入锁的状态的
+   */
   private volatile int state;
   protected final int getState() { return state; }
   protected final void setState(int newState) { state = newState; }
@@ -68,7 +71,7 @@ public class AbstractQueuedSynchronizerCopy extends AbstractOwnableSynchronizerC
   static final long spinForTimeoutThreshold = 1000L;
 
   /**
-   * 双向链表，并且原子操作链表添加
+   * 双向链表  在尾部添加一个节点，有一个dummy的node为初始值
    * 1.链表初始化为一个new node
    * 2.链表操作包括设置head和tail都是原子操作
    * 逻辑
@@ -92,7 +95,7 @@ public class AbstractQueuedSynchronizerCopy extends AbstractOwnableSynchronizerC
   }
 
   /**
-   * 逻辑
+   * 逻辑 增加一个等待的节点，节点的Node中包含了Thread对象
    * 1.新建一个节点，节点成员是currentThread和下一个节点
    * 2.把新建到节点添加到链表尾部
    * 3.返回的是这个新建的节点
@@ -121,9 +124,9 @@ public class AbstractQueuedSynchronizerCopy extends AbstractOwnableSynchronizerC
   }
 
   /**
-   * 逻辑  唤醒node（不包括当前节点）后面的节点中休眠的线程
+   * 逻辑  唤醒一个node（不包括当前节点和waitStatus为Cancelled的节点）后面的节点中休眠的线程
    * 1.当前节点node.waitStatus < 0 即非取消状态则设置为0，失败了也没关系
-   * 2.如果下一个节点node.next.waitStatus = cancelled状态,则从tail开始查找不是cancelled状态的节点
+   * 2.如果下一个节点node.next.waitStatus != cancelled状态的节点,则从tail开始查找不是cancelled状态的节点
    * 3.如果找到了不是cancelled状态的节点就unpark(node.thread)，即唤醒在这个节点等待的线程
    */
   private void unparkSuccessor(Node node) {
@@ -144,6 +147,12 @@ public class AbstractQueuedSynchronizerCopy extends AbstractOwnableSynchronizerC
       LockSupport.unpark(s.thread);
   }
 
+  /**
+   * 逻辑 就是signal信号的线程唤醒动作
+   * 1.初始化了且不是dummy的node
+   * 2.如果waitStatus==0(0是中间的状态
+   * 3.如果head变更，就会循环执行，就是释放过程中head的状态需要保持一致
+   */
   private void doReleaseShared() {
     for (;;) {
       Node h = head;
@@ -161,6 +170,94 @@ public class AbstractQueuedSynchronizerCopy extends AbstractOwnableSynchronizerC
         break;
     }
   }
+
+  private void setHeadAndPropagate(Node node, int propagate) {
+    Node h = head;
+    setHead(node);
+    if (propagate > 0 || h == null || h.waitStatus < 0 || (h = head) == null || h.waitStatus < 0) {
+      Node s = node.next;
+      if (s == null || s.isShared())
+        doReleaseShared();
+    }
+  }
+
+  /**
+   * node:清除状态并出队
+   * 清除node的状态
+   * 1.node.thread = null
+   * 2.node.waitStatus = Node.CANCELLED
+   * 出队逻辑
+   * 1.跳过cancelled的节点
+   *   pred = node的前面的节点中第一个waitStatus != cancelled的节点
+   *   node.prev = pred
+   *   predNext = pred.next # 是cancelled的node或node
+   * 2.出队逻辑以pred处于哪个位置分为3种情况
+   *   2.1 node == tail && CAS.this.tail = pred  # node == tail的情况，设置tail为pred
+   *              CAS.pred.next = null           # 原子操作的设置pred.next = null
+   *   2.2 条件,3个条件都必须满足，即&&
+   *          1.pred ！= head
+   *          2.pred.waitStatus == signal 或者
+   *            pred.waitStatus <= 0 && CAS.pred.waitStatus = signal
+   *          3.pred.thread != null
+   *       条件满足后则执行如果
+   *       node.next != null && nodex.next.waitStatus <= 0
+   *       则 CAS.pred.next = next
+   *          node.next = node 便于GC
+   *   2.3 其他条件
+   *       唤醒node后面的节点并且node.next = node
+   */
+  private void cancelAcquire(Node node) {
+    if (node == null)
+      return;
+
+    node.thread = null;
+    Node pred = node.prev;
+
+    // 跳过取消的节点，并且将node.prev指向最终不是cancelled的节点
+    while (pred.waitStatus > 0)
+      node.prev = pred = pred.prev;
+    Node predNext = pred.next;
+    node.waitStatus = Node.CANCELLED;
+
+    // 在尾部就直接移除自己
+    if (node == tail && compareAndSetTail(node, pred)) {
+      compareAndSetNext(pred, predNext, null);
+    } else {
+      int ws;
+      if (pred != head && ((ws = pred.waitStatus) == Node.SIGNAL || (ws <= 0 && compareAndSetWaitStatus(pred, ws, Node
+              .SIGNAL))) && pred.thread != null) {
+        Node next = node.next;
+        if (next != null && next.waitStatus <= 0)
+          compareAndSetNext(pred, predNext, next);
+      } else {
+        unparkSuccessor(node);
+      }
+      node.next = node; // help GC
+    }
+  }
+
+  /**
+   * 逻辑  如果线程应该block就返回true
+   * 1.pred.waitStatus = signal  return true  前驱节点状态是signal就block当前节点（返回true），否则就返回false
+   * 2.循环清理cancelled的前驱节点，直到有一个pred不是cancelled的状态
+   * 3.前驱节点如果是非cancelled非signal。就CAS.pred.waitStatus = signal,不关心是否成功
+   */
+  private static boolean shouldParkAfterFailedAcquire(Node pred, Node node) {
+    int ws = pred.waitStatus;
+    if (ws == Node.SIGNAL)
+      return true;
+    if (ws > 0) {
+      do {
+        node.prev = pred = pred.prev;
+      } while (pred.waitStatus > 0);
+      pred.next = node;
+    } else {
+      compareAndSetWaitStatus(pred, ws, Node.SIGNAL);
+    }
+    return false;
+  }
+
+  
 
   private static final Unsafe unsafe = Unsafe.getUnsafe();
   private static final long stateOffset;
