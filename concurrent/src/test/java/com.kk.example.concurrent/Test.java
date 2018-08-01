74c1432ad677183935a1bf6cb4ee13f062108c4c
weijin <weijin729@gmail.com>
2018-08-01 22:08:41 +0800

    no comment

--- concurrent/src/test/java/com.kk.example.concurrent/Test.java
+++ concurrent/src/test/java/com.kk.example.concurrent/Test.java
@@ -5,10 +5,7 @@ import java.util.concurrent.FutureTask;
 public class Test {
 
     public static void main(String[] args) {
-        int k = 5;
-        for (int i = 0; q = k; k++) {
 
-        }
     }
 
 }
